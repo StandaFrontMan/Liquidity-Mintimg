fb2561884dd0264a2f37e68d1dfea7bfa3fcc1fd
stas <stas@192.168.1.5>
2026-02-11 23:28:42 +0100

    feat/contract: shadcn instalation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,12 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
-      <div className="text-red-400">initial</div>
+      <div className="text-red-400">
+        <Button>asdasd</Button>
+      </div>
     </>
   );
 }
