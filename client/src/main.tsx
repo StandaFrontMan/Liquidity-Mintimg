6badebef103759367e341b11f625e45fdfaba070
stas <stas@192.168.1.5>
2026-02-12 04:22:51 +0100

    feat/client: providers

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { WagmiProvider } from "wagmi";
+import { config } from "./config/wagmi.ts";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-createRoot(document.getElementById('root')!).render(
+const query = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={query}>
+        <App />
+      </QueryClientProvider>
+    </WagmiProvider>
   </StrictMode>,
-)
+);
